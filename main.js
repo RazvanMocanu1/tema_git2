8213bf25debb2b4002a19654e47a204a9a8f405e
RazvanMocanu1 <razvanmocanu2009@gmail.com>
2023-08-26 09:15:15 +0300

    inceput butoane

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 const burger = document.getElementById("burger1");
 const pizza = document.getElementById("pizza1");
 const spaghetti = document.getElementById("spaghetti1");
+const Pburger = document.getElementById("burgerh1");
+const Ppizza = document.getElementById("pizzah1");
+const Pspaghetti = document.getElementById("spaghettih1");
 
 function burger1 (){
 
-
+    burger.style.display="none";
+    Pburger.innerText="Pret : 23.50 lei";
 
 }
 
@@ -12,7 +16,8 @@ burger.addEventListener("click", burger1);
 
 function pizza1 (){
 
-
+    pizza.style.display="none";
+    Ppizza.innerText="Pret : 28 lei";
 
 }
 
@@ -20,7 +25,8 @@ pizza.addEventListener("click", pizza1);
 
 function spaghetti1 (){
 
-
+    spaghetti.style.display="none";
+    Pspaghetti.innerText="Pret : 18.99 lei"
 
 }
 
